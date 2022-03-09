4653ca121df778fbc19106eaf9edb6726d2bb2da
Ivan Cvetić <74881141+sivizdral@users.noreply.github.com>
2022-03-09 19:25:56 +0100

    Add files via upload

---
+++ baze/banka_filijala.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: localhost    Database: banka
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `filijala`
+--
+
+DROP TABLE IF EXISTS `filijala`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `filijala` (
+  `IdFil` int NOT NULL AUTO_INCREMENT,
+  `Naziv` varchar(45) NOT NULL,
+  `Adresa` varchar(45) NOT NULL,
+  `Mesto` int NOT NULL,
+  PRIMARY KEY (`IdFil`),
+  KEY `filijala_mesto_fk_idx` (`Mesto`),
+  CONSTRAINT `filijala_mesto_fk` FOREIGN KEY (`Mesto`) REFERENCES `mesto` (`IdMes`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `filijala`
+--
+
+LOCK TABLES `filijala` WRITE;
+/*!40000 ALTER TABLE `filijala` DISABLE KEYS */;
+INSERT INTO `filijala` VALUES (1,'UniCredit Kraljevo','Oktobarskih zrtava 22',2),(2,'UniCredit Uzice','Dimitrija Tucovića 97',17),(3,'UniCredit Sombor','Stepe Stepanovića 20',15),(4,'UniCredit Novi Sad 1','Danila Kisa 3',11),(5,'UniCredit Novi Sad 2','Katolicka porta 2',11),(6,'UniCredit Blokovi','Jurija Gagarina 30',3),(7,'UniCredit Merkator','Stari Merkator',3),(8,'UniCredit Belville','Belville',3),(9,'UniCredit Pancevo','Radomira Putnika 22',16),(10,'UniCredit Kragujevac','Kosovska 4',1);
+/*!40000 ALTER TABLE `filijala` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-02-03  3:31:34
